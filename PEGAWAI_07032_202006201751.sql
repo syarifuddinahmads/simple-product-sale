3ff361af898815d9c37efa24944a14343ac7e62a
Ahmad Syarifuddin <udin@gaeasystem.com>
2020-06-21 16:02:14 +0700

    remove sql

--- PEGAWAI_07032_202006201751.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO FITRIARISQINA_07032.PEGAWAI_07032 (ID_PEGAWAI,ID_USER,NAMA_PEGAWAI,ALAMAT) VALUES 
-(6,6,'tiara','Tuban')
-,(7,7,'nadia','Madura')
-,(8,8,'dini','Surabaya')
-,(9,9,'Nini','Madura')
-,(10,10,'Tika','Tuban')
-;
